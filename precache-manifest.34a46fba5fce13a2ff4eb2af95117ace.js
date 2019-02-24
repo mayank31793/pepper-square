a3b83b8a0e1faff5e51d4608b56a74bbd5903cd8
mayank31793 <mayank31793@gmail.com>
2019-02-24 23:58:02 +0530

    Updates

---
+++ precache-manifest.34a46fba5fce13a2ff4eb2af95117ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "271c20a8e0c701415dc7",
+    "url": "/react/static/js/main.271c20a8.chunk.js"
+  },
+  {
+    "revision": "51e083a63843d4a277c7",
+    "url": "/react/static/js/1.51e083a6.chunk.js"
+  },
+  {
+    "revision": "271c20a8e0c701415dc7",
+    "url": "/react/static/css/main.01837a08.chunk.css"
+  },
+  {
+    "revision": "d858ac11cff7bb4ef599a6f6aa598005",
+    "url": "/react/index.html"
+  }
+];
